8eabf26a62f5cb6c34ee6403bf9e275c7cb6bae0
Samuel-Rosiji <youremail@example.com>
2026-09-06 23:29:21 +0100

    Migrated to MongoDB Atlas and fixed main.py

--- main.py
+++ main.py
@@ -8,22 +8,20 @@ from fetch_passing import fetch_player_passing
 
 
 def main():
-    try:
-        fetch_two_man_lineups()
-        db["passes"].drop()
-        for doc in get_top_duos(limit=50):
-            group_arr =  doc['GROUP_ID'].split("-")
-            team = doc['TEAM_ID']
-            player_1 =  int(group_arr[1])
-            teammate_2 = int(group_arr[2])
-            passes_made, passes_received = fetch_player_passing(player_1, team, teammate_2)
-            if passes_made is not None:
-                insert_passing(passes_made, passes_received)
-            time.sleep(1)
+    
+    fetch_two_man_lineups()
+    db["passes"].drop()
+    for doc in get_top_duos(limit=50):
+        group_arr =  doc['GROUP_ID'].split("-")
+        team = doc['TEAM_ID']
+        player_1 =  int(group_arr[1])
+        teammate_2 = int(group_arr[2])
+        passes_made, passes_received = fetch_player_passing(player_1, team, teammate_2)
+        if passes_made is not None:
+            insert_passing(passes_made, passes_received)
+        time.sleep(1)
 
-    except Exception as e:
-            print(f"Failed for player {player_id}: {e}")
-            return None, None
+    
 
 
 
